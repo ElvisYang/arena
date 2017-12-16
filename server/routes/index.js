25621a6da6252ad50fa2844f97320da7329a82d1
Jake Cattrall <krazyjakee@gmail.com>
2017-12-16 14:31:50 +0100

    better development configurations

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,14 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import ssr from './ssr';
+import api from './api';
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use('/api', api);
 app.use('/*', ssr);
 
 app.listen(3000, () => {
--- webpack/client.dev.js
+++ webpack/client.dev.js
@@ -4,62 +4,62 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  context: path.join(__dirname, '../client'),
-  devtool: 'inline-source-map',
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './src/index.js',
-    './res/scss/main.scss',
-  ],
-  output: {
-    path: path.join(__dirname, '../server/public'),
-    filename: './js/index.js',
-    publicPath: '/',
-  },
-  devServer: {
-    hot: true,
-    publicPath: '/',
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'es2015', 'stage-1'],
-        },
-      },
-    },
-    {
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-    },
-    {
-      test: /\.(png|jpg|gif)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {},
-        },
-      ],
-    },
-    {
-      test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-      loader: 'file-loader?name=fonts/[name].[ext]',
-    },
+    context: path.join(__dirname, '../client'),
+    devtool: 'inline-source-map',
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.js',
+        './res/scss/main.scss',
     ],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.join(__dirname, '../server/views/index.dev.ejs'),
-      inject: false,
-    }),
-  ],
+    output: {
+        path: path.join(__dirname, '../server/public'),
+        filename: './js/index.js',
+        publicPath: '/',
+    },
+    devServer: {
+        hot: true,
+        publicPath: '/',
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react', 'es2015', 'stage-1'],
+                },
+            },
+        },
+        {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {},
+                },
+            ],
+        },
+        {
+            test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+            loader: 'file-loader?name=fonts/[name].[ext]',
+        },
+        ],
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname, '../server/views/index.dev.ejs'),
+            inject: false,
+        }),
+    ],
 };
